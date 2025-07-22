67f42ec7916f7d6c90ceb84ae99c2e18b45fd0c2
Jouvan <jouvan294@gmail.com>
2025-07-23 01:16:29 +0700

    Update All Files

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@ using namespace std;
 
 int main() {
 
-    //FSM robotFSM(2000);
-    //robotFSM.start();
-    
-    FSM robotFSM;
+    FSM robotFSM(2000);
+    robotFSM.start();
     
     return 0;
 }
